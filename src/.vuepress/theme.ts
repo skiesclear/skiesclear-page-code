17117849043fb444c1af7f9f463b783b5c4675bc
skiesclear <abc3060647189@outlook.com>
2024-09-24 22:11:25 +0800

    deploy-success

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -4,22 +4,22 @@ import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
 export default hopeTheme({
-  hostname: "https://mister-hope.github.io",
+  hostname: "clearday",
   //////
   darkmode: "auto",
   // fullscreen: true,
   pure: true,
+  breadcrumb: false,
   //////
   author: {
-    name: "Mr.Hope",
-    url: "https://mister-hope.com",
+    name: "clearday",
   },
 
   iconAssets: "fontawesome-with-brands",
 
-  logo: "https://theme-hope-assets.vuejs.press/logo.svg",
+  logo: "/bear.png",
 
-  repo: "vuepress-theme-hope/vuepress-theme-hope",
+  repo: "skiesclear/skiesclear-page-code",
 
   docsDir: "src",
 
@@ -29,55 +29,21 @@ export default hopeTheme({
   // sidebar
   sidebar,
 
-  footer: "Default footer",
+  footer: "clearday--",
 
   displayFooter: true,
 
-  encrypt: {
-    config: {
-      "/demo/encrypt.html": ["1234"],
-    },
-  },
 
   blog: {
     description: "A FrontEnd programmer",
     intro: "/intro.html",
     medias: {
-      Baidu: "https://example.com",
       BiliBili: "https://example.com",
-      Bitbucket: "https://example.com",
-      Dingding: "https://example.com",
-      Discord: "https://example.com",
-      Dribbble: "https://example.com",
       Email: "mailto:info@example.com",
-      Evernote: "https://example.com",
-      Facebook: "https://example.com",
-      Flipboard: "https://example.com",
       Gitee: "https://example.com",
       GitHub: "https://example.com",
-      Gitlab: "https://example.com",
-      Gmail: "mailto:info@example.com",
-      Instagram: "https://example.com",
-      Lark: "https://example.com",
-      Lines: "https://example.com",
-      Linkedin: "https://example.com",
-      Pinterest: "https://example.com",
-      Pocket: "https://example.com",
-      QQ: "https://example.com",
-      Qzone: "https://example.com",
-      Reddit: "https://example.com",
-      Rss: "https://example.com",
-      Steam: "https://example.com",
-      Twitter: "https://example.com",
-      Wechat: "https://example.com",
-      Weibo: "https://example.com",
-      Whatsapp: "https://example.com",
-      Youtube: "https://example.com",
-      Zhihu: "https://example.com",
-      VuePressThemeHope: {
-        icon: "https://theme-hope-assets.vuejs.press/logo.svg",
-        link: "https://theme-hope.vuejs.press",
-      },
+
+
     },
   },
 
@@ -98,7 +64,14 @@ export default hopeTheme({
     //   provider: "Waline",
     //   serverURL: "https://waline-comment.vuejs.press",
     // },
-
+    shiki: {
+      theme: "nord",
+    },
+    copyCode: {
+      showInMobile: true,
+      duration: 1000,
+    },
+    photoSwipe: false,
 
     components: {
       components: ["Badge", "VPCard"],
